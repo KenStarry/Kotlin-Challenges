877937d547842cd6e4739b7f52e450ef9bf67a9e
KenStarry <starrycodes@gmail.com>
2022-06-26 00:49:56 +0300

    Question 2

---
+++ src/main/kotlin/basics/Read_Numbers.kt
@@ -0,0 +1,13 @@
+package basics
+
+import java.util.Scanner
+
+fun main() {
+
+    //  Reading from the Scanner class
+    val reader = Scanner(System.`in`)
+
+    val input = reader.nextInt()
+    println("Your value is $input")
+
+}
--- src/main/kotlin/basics/Read_User_Input.kt
+++ src/main/kotlin/basics/Read_User_Input.kt
@@ -2,5 +2,6 @@ fun main(args: Array<String>) {
 
     print("Enter username : ")
     val username: String = readLine()!!
+
     println("Username : $username")
 }
